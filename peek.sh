7266fe37909aea664ae9d32294fae7998d2591f1
Jingqi Fu <fujingqi920@gmail.com>
2024-10-31 14:14:09 +0100

    Update peek.sh with new changes

--- peek.sh
+++ peek.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-filename=$1
-lines=$2
+if [[ -z $2 ]]; then
+  LINES=3
+else
+  LINES=$2
+fi
 
-head -n $lines "$filename"
-echo "..."
-tail -n $lines "$filename"
+TOTALLINES=$(cat "$1" | wc -l)
+
+if [[ $TOTALLINES -le $((2 * LINES)) ]];
+then
+  cat "$1"
+else
+  echo "The first $LINES and last $LINES lines."
+  head -n "$LINES" "$1"
+  echo "..."
+  tail -n "$LINES" "$1"
+fi
